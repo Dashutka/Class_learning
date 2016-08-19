d25aa9200ff7ad1788a7d2073e86cac69e5f699c
Dashutka <1@gmail.com>
2016-08-19 21:14:39 +0300

    add new
    new projecte

---
+++ Class_learning/Classes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_learning
+{
+    class Classes
+    {
+        public int AddTen(ref int a) {
+            a += a;
+            return a;
+        }
+        // out for return more that 2 params
+
+        public int AddAge(int[] test) {
+            int count = 0;
+            for (int i = 0; i < test.Length; i++)
+            {
+                count += test[i];
+            }
+            return count;
+        }
+
+
+
+        /*  public double Average(params double[] array) {
+              double avg = 0;
+              foreach (var d in array)
+              {
+
+                  return d / array.Length;
+              }
+       }*/
+    }
+}
---
+++ Class_learning/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_learning
+{
+    public class Program
+    {
+        static void Main(string[] args) {
+
+            Classes test = new Classes();
+
+            int[] array = new int[10] { 10, 2, 3, 5, 6, 4, 5, 4, 6, 10 };
+            //  Console.WriteLine($"{array.Sum([]10)}");
+
+            int a = 4;
+            test.AddTen(ref a);
+            Console.WriteLine($"{a}");
+
+            if (int.TryParse("12347", out a))
+            {
+                Console.WriteLine(a);
+
+            } else
+            {
+                Console.WriteLine("error");
+            }
+            Console.ReadLine();
+
+            string aa = "abc";
+            MethodString(aa);
+        }
+
+        static void MethodString(string test) {
+           // test[0] = test;
+            test += "123";
+            Console.WriteLine(test);
+        }
+    }
+}
